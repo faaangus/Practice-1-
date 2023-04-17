0b82638e720220117d274648f8aa1ea9c1fe36ce
yifang <jaefangg@gmail.com>
2023-04-18 01:56:49 +0800

    commit python files

---
+++ spyder/integers_python.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#print integers without a loop
+print(1,2,3,4,5,6,7,8,9,10)
+
+#print integers 1 to 10 using a loop
+for i in range(1, 11, 2):
+    print(i, end=" ")
+
+
+#print integers 1 to 10 using a while loop
+j = 1
+while (j<=10):
+     print(j)
+     j = j + 1
+      # j += 1
+      
